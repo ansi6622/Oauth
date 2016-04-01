b9e4a7202d027df193779fad1eba76157677305d
Anthony Simpson <ansi6622@colorado.edu>
2016-03-31 21:33:08 -0600

    everything works as it should from what i can tell, pulls the picture etc...

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
+router.get('/', function(req, res, next) {
+
+    console.log(req.isAuthenticated());
+    console.log(req.session.passport.user.token);
+    unirest.get('https://api.linkedin.com/v1/people/~:(id,num-connections,picture-url)')
+      .header('Authorization', 'Bearer ' + req.session.passport.user.token)
+      .header('x-li-format', 'json')
+      .end(function (response) {
+
+        res.render('index', { profile: response.body, title: 'LinkedIN O-Auths' });
+        console.log(req.isAuthenticated());
+
+      })
+
+});
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   console.log(req.session.passport.user);
 //   res.render('index', { title: 'Express', user: req.session.passport.user });
 // });
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LinkedIN O-Auth' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'LinkedIN O-Auth' });
+// });
 // router.get('/logout', function(req, res) {
 //     res.clearCookie('userID');
 //     res.clearCookie('userAdmin');
